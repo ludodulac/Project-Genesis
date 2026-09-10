0171d6c264e3054f7e86033a399b6c9cca9d41d7
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-09-10 16:04:57 +0200

    Route EXP-024 trajectory probe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Phaser from 'phaser';
 import './style.css';
 import { WorldScene } from './presentation/WorldScene';
+import { TrajectoryScene } from './presentation/TrajectoryScene';
+
+const probe = new URLSearchParams(window.location.search).get('probe');
+const scene = probe === 'trajectory' ? TrajectoryScene : WorldScene;
 
 new Phaser.Game({
   type: Phaser.AUTO,
@@ -8,7 +12,7 @@ new Phaser.Game({
   width: 390,
   height: 760,
   backgroundColor: '#dff7ff',
-  scene: [WorldScene],
+  scene: [scene],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
